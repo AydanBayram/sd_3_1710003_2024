1e8700a633f1eadfd3ee2c55cb40b3ab0ba45443
AydanBayram <aydanbyram9@gmail.com>
2024-03-01 13:32:08 +0200

    Update h1.py

--- First_project/h1.py
+++ First_project/h1.py
@@ -11,3 +11,10 @@ y = "John"
 # type indica o tipo da variavel
 print(type(x))
 print(type(y))
+x = 5
+y = "John"
+Y = "case-sensitve"
+# type indica o tipo da variavel
+print(type(x))
+print(type(y))
+print(x,y,Y)
