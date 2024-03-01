c99808cb7040ef0c920e51b6c2951192d1a09bcd
AydanBayram <aydanbyram9@gmail.com>
2024-03-01 13:36:35 +0200

    Update h1.py

--- First_project/h1.py
+++ First_project/h1.py
@@ -1,20 +1,46 @@
-
-print("ola mundo!")
-x=3
-print("x= ",x)
+# first example
+print("\nola mundo!")
+x = 3
 y = 5
 y = "John"
-print("x= ",x)
-print("y= ",y)
-x = 5
-y = "John"
-# type indica o tipo da variavel
+print("x= ", x)
+print("y= ", y, "\n")
+
+# second example
 print(type(x))
-print(type(y))
+print(type(y), "\n")
+
+# third example
 x = 5
 y = "John"
 Y = "case-sensitve"
-# type indica o tipo da variavel
-print(type(x))
-print(type(y))
-print(x,y,Y)
+print(x, y, Y, "\n")
+
+# fourth example
+x = "Hello"
+print(x)
+y = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+print(y, "\n")
+
+# fifth example
+x = "Python is "
+y = "awesome"
+z = x + y
+x = 5
+y = 10
+print(z, ",", x + y, "\n")
+
+# sixth example
+x = 5
+y = 2
+if x > y:
+    print("x é maior que y")
+else:
+    print("x não é maior que y")
+i = 1
+while i < 6:
+    print(i)
+    i += 1
